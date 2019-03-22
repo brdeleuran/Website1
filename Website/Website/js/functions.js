b93bab36865d8545b57f9dba6d57821273e5f3d1
Bjørn Deleuran <b.r.deleuran@hotmail.com>
2019-03-22 10:58:39 +0100

    First Commit

---
+++ Website/Website/js/functions.js
@@ -0,0 +1,12 @@
+﻿jQuery(document).ready(function ($) {
+
+    window.onscroll = function () { myFunction(); change(); change1(); };
+
+    function myFunction() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }
+
+});
